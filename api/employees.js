7748805e5854375352edc365c3aa02dd230a7353
Roger Zavala <rogeliozavala15@gmail.com>
2020-05-24 15:02:35 -0700

    created param route for employee id

--- api/employees.js
+++ api/employees.js
@@ -3,6 +3,23 @@ const employeeRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+// Checks if employee exist, if so sends employee if not sends status 404
+employeeRouter.param('id', (req, res, next, id) =>{
+    const sql = 'SELECT * FROM Employee WHERE id = $employeeId'
+    
+    db.get(sql, {$employeeId: id}, (error, employee) => {
+        if(error){
+            next(error);
+        }else if(employee){
+           req.employee = employee;
+           next();
+        }else{
+            res.sendStatus(404);
+        }
+    });
+
+});
+
 // retrieves all current employees from database
 employeeRouter.get('/', (req, res, next) =>{
     db.all('SELECT * FROM Employee WHERE is_current_employee = 1', (error, employee) => {
@@ -11,8 +28,13 @@ employeeRouter.get('/', (req, res, next) =>{
         }else{
             res.status(200).json({employees: employee});
         }
-
     });
 });
+
+employeeRouter.get('/:id', (req, res, next) => {
+    res.status(200).json({employee: req.employee});
+});
+ 
+
 
 module.exports = employeeRouter;
