d22267c8012d646b39f58f7ba986dc95bd15ba7d
Roger Zavala <rogeliozavala15@gmail.com>
2020-05-26 14:03:17 -0700

    created a new post route for menus

--- api/menu.js
+++ api/menu.js
@@ -4,6 +4,7 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 menuRouter.param('menuId', (req, res, next, menuId) => {
+    
     const sql = 'Select * FROM Menu WHERE id = $menuId';
     db.get(sql,{$menuId: menuId}, (error, menu) =>{
         if(error){
@@ -27,6 +28,27 @@ menuRouter.get('/', (req, res, next) => {
     });
 });
 
-menuRouter.get();
+menuRouter.get('/:menuId', (req, res, next) => {
+    res.status(200).json({menu: req.menu});
+});
+
+menuRouter.post('/', (req, res, next) => {
+    const title = req.body.menu.title;
+    if(!title){
+        return res.sendStatus(400);
+    }
+
+    const sql = 'INSERT INTO Menu (title) VALUES($title)';
+    db.run(sql, {$title: title}, function(error){
+        if(error){
+            next(error);
+        }else{
+            db.get(`SELECT * FROM Menu WHERE id = ${this.lastID}`, (error, menu) =>{
+                res.status(201).json({menu: menu});
+            });
+        }
+    });
+
+});
 
 module.exports = menuRouter;
