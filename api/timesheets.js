67c5cbfaf197005b325b9635c4f5907f41b39c48
Roger Zavala <rogeliozavala15@gmail.com>
2020-05-25 22:16:20 -0700

    finished put route to update employee timesheet

--- api/timesheets.js
+++ api/timesheets.js
@@ -4,6 +4,20 @@ const timesheetRouter =  express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+timesheetRouter.param('timesheetId', (req, res, next, timesheetId) =>{
+    const sql = `SELECT * FROM Timesheet WHERE Timesheet.id = $timesheetId`;
+    
+    db.get(sql,{$timesheetId:timesheetId},(error, timesheet) =>{
+        if(error){
+            next(error);
+        }else if(timesheet){
+            next();
+        }else{
+            res.sendStatus(404);
+        }
+    });
+});
+
 timesheetRouter.get('/', (req, res, next) => {
     // console.log(req.params.id)
     db.all('SELECT * FROM Timesheet WHERE Timesheet.employee_id = $employeeId',{$employeeId: req.params.employeeId},
@@ -44,6 +58,36 @@ timesheetRouter.post('/', (req, res, next) => {
         }
     });
 
+});
+
+timesheetRouter.put('/:timesheetId', (req, res, next) => {
+    const hours = req.body.timesheet.hours,
+          rate = req.body.timesheet.rate,
+          date = req.body.timesheet.date,
+          employeeId = req.params.employeeId;
+
+    if(!hours || !rate || !date){
+        return res.sendStatus(400);
+    }
+
+    const sql = 'UPDATE Timesheet SET hours = $hours, rate = $rate, date = $date, employee_id = $employeeId WHERE Timesheet.id = $timesheetId';
+    const values = {
+        $hours: hours,
+        $rate: rate,
+        $date: date,
+        $employeeId: employeeId,
+        $timesheetId: req.params.timesheetId
+    };
+
+    db.run(sql, values, function(error){
+        if(error){
+            next(error);
+        }else{
+            db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = ${req.params.timesheetId}`, (error, timesheet) => {
+                res.status(200).json({timesheet: timesheet});
+            });
+        }
+    });
 });
 
 module.exports = timesheetRouter;
