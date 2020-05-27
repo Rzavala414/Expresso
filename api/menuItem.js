ecb1c230fd90ebd905c53aa1748101e06f6793ed
Roger Zavala <rogeliozavala15@gmail.com>
2020-05-27 16:51:19 -0700

    created new parms middleware for menuItmes

--- api/menuItem.js
+++ api/menuItem.js
@@ -3,20 +3,20 @@ const menuItemsRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
-menuItemsRouter.param('menuItemId', (req, res, next, menuItemId) => {
-    const sql = 'SELECT * FROM MenuItem WHERE MenuItem.id = $menuItemId';
+// menuItemsRouter.param('menuItemId', (req, res, next, menuItemId) => {
+//     const sql = 'SELECT * FROM MenuItem WHERE MenuItem.id = $menuItemId';
     
-    db.get(sql, {$menuItemId:menuItemId}, (error, menuItem) => {
-        if(error){
-            next(error);
-        }else if(menuItem){
-            req.menuItem = menuItem;
-            next();
-        }else{
-            res.sendStatus(404);
-        }
-    })
-});
+//     db.get(sql, {$menuItemId:menuItemId}, (error, menuItem) => {
+//         if(error){
+//             next(error);
+//         }else if(menuItem){
+//             req.menuItem = menuItem;
+//             next();
+//         }else{
+//             res.sendStatus(404);
+//         }
+//     })
+// });
 
 menuItemsRouter.get('/', (req, res, next) => {
     const sql = 'SELECT * FROM MenuItem WHERE MenuItem.menu_id = $menuId';
